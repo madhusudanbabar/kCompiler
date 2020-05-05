d9a20b0a6e437fd49bab4db4969a190ff8397f8a
madhusudan babar <madhusudanbabar@gmail.com>
2020-05-05 09:00:25 +0530

    file upload test

--- server.js
+++ server.js
@@ -1,37 +1,36 @@
 const express = require("express")
 const app = express()
-const spawn = require("child_process").spawnSync;
-const sleep = require("sleep")
-app.use(express.urlencoded({ extended:true}))
-const fs = require("fs")
+const path = require("path")
 const multer = require("multer")
+
 const storage = multer.diskStorage({
-    destination: function(req, file, cb) {
-        cb(null, 'uploads/');
-    },
-
-    // By default, multer removes file extensions so let's add them back
-    filename: function(req, file, cb) {
-        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+    destination: "./uploads",
+    filename: function (req, file, cb) {
+        fileName = path.basename(file.originalname)
+        console.log(fileName);
+        
+        cb(null, fileName)
     }
-});
-app.use(express.json())
-
-let upload = multer({ storage: storage }).single('profile_pic');
-
-
-app.listen(3000,()=>{
-    console.log("running on port 3000");
-    
 })
 
-app.post("*", (req, res)=>{
-    upload(req, res,(err)=>{
-        res.send(err)
-        console.log(req.body);    
-    
-    sleep.sleep(2);
-    // console.log(lang);
-    // res.send("recieved")
+app.use(express.json())
+
+var upload = multer({
+    storage: storage
+}).single("file")
+
+app.post("/upload", (req, res) => {
+    upload(req, res, (err) => {
+        if (err) {
+            console.log(err);
+            res.json(err)
+        }
+        else {
+            res.redirect("")
+        }
     })
+})
+
+app.listen(3000, () => {
+    console.log("running on port 3000");
 })
