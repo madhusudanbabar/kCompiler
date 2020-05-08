4bdb190d3016d613fef558785b47c515058d5404
madhusudan babar <madhusudanbabar@gmail.com>
2020-05-08 15:17:47 +0530

    works fine for python language

--- server.js
+++ server.js
@@ -3,16 +3,17 @@ const app = express()
 const path = require("path")
 const multer = require("multer")
 const bodyParser = require('body-parser');
+const fs = require('fs');
+const { spawn, spawnSync } = require("child_process")
 
 app.use(bodyParser.json())
-
 
 const storage = multer.diskStorage({
     destination: "./uploads",
     filename: function (req, file, cb) {
         fileName = path.basename(file.originalname)
         console.log(fileName);
-        
+
         cb(null, fileName)
     }
 })
@@ -26,19 +27,83 @@ var upload = multer({
 }).single("file")
 
 app.post("/upload", (req, res) => {
-    // console.log(req);
+    upload(req, res, (err) => {
+        lang = req.body.lang;
+        // console.log(lang);
+        var tmpFile;
+        switch (lang) {
+            case "c":
+                tmpFile = "tmp.c";
+                break;
+            case "c++":
+                tmpFile = "tmp.cpp";
+                break;
+            case "python":
+                tmpFile = "tmp.py";
+                break;
+        }
 
-    upload(req, res, (err) => {   
-        console.log(req.body);
-        if (err) {
-            console.log(err);
-            res.json(err)
+
+        // try {
+        //     fs.unlinkSync(path.join(__dirname, "uploads", tmpFile));
+        // } catch (error) {
+        //     // res.json(error)
+        // }
+
+        console.log("im here");
+        
+        try {
+            fs.writeFileSync(path.join(__dirname, "uploads", tmpFile), req.body.code);
+            console.log("file written");
+            
+        } catch (error) {
+            res.json(error)
         }
-        else {
-            res.json("file recieved");
+        var compiler = null;
+        var op = new Object;
+        if (lang == "c") {
+            try {
+                mid = spawnSync('gcc', ['tmp.c']);
+                console.log("file written successfully");
+            } catch (error) {
+                res.json(error)
+            }
+            compiler = spawn('./a.out');
+        } else if (lang == "cpp") {
+            try {
+                mid = spawnSync('g++', ['tmp.cpp']);
+                console.log("file written successfully");
+            } catch (error) {
+                res.json(error)
+            }
+            compiler = spawn('./a.out');
         }
+        else if (lang == "python") {
+            compiler = spawn('python3', [path.join(__dirname, "uploads/") + 'tmp.py']);
+        }
+        compiler.stdout.on('data', (data) => {
+            str = String(data)
+            console.log(`stdout: ${str}`);
+            // res.write(JSON.stringify(`process exited with code ${str}`))
+            // res.write(JSON.stringify("stdout"))
+            op.out = str;
+        });
+
+        compiler.stderr.on('data', (data) => {
+            str = String(data)
+            console.error(`stderr: ${str}`);
+            // res.write(JSON.stringify(str));
+            // res.send("stderr")
+            op.err = str;
+        });
+
+        compiler.on('close', (code) => {
+            console.log(`process exited with code ${code}`);
+            // res.end(JSON.stringify(`process exited with code ${code}`));
+            res.status(200).json(op)
+        });
     })
-})
+});
 
 app.listen(3000, () => {
     console.log("running on port 3000");
