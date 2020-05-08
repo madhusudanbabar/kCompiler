a8ce98035372dd508296e2dc08d3c1e4f95c704f
madhusudan babar <madhusudanbabar@gmail.com>
2020-05-08 18:19:58 +0530

    python execution works well

--- server.js
+++ server.js
@@ -1,110 +1,104 @@
 const express = require("express")
 const app = express()
 const path = require("path")
-const multer = require("multer")
 const bodyParser = require('body-parser');
+const multer = require('multer');
 const fs = require('fs');
 const { spawn, spawnSync } = require("child_process")
 
 app.use(bodyParser.json())
-
-const storage = multer.diskStorage({
-    destination: "./uploads",
-    filename: function (req, file, cb) {
-        fileName = path.basename(file.originalname)
-        console.log(fileName);
-
-        cb(null, fileName)
-    }
-})
+app.use(multer().single())
 
 app.use(express.static(path.join(__dirname, "dist", "compiler")))
-
-app.use(express.json())
-
-var upload = multer({
-    storage: storage
-}).single("file")
+var programFile;
 
 app.post("/upload", (req, res) => {
-    upload(req, res, (err) => {
-        lang = req.body.lang;
-        // console.log(lang);
-        var tmpFile;
-        switch (lang) {
-            case "c":
-                tmpFile = "tmp.c";
-                break;
-            case "c++":
-                tmpFile = "tmp.cpp";
-                break;
-            case "python":
-                tmpFile = "tmp.py";
-                break;
-        }
+    var lang = req.body.lang;
+    console.log(lang);
+    
+    switch (lang) {
+        case "c":
+            programFile = path.join(__dirname,"uploads","tmp.c");
+            break;
+        case "c++":
+            programFile = path.join(__dirname,"uploads","tmp.cpp");
+            break;
+        case "python":
+            programFile = path.join(__dirname,"uploads","tmp.py");
+            break;
+    }
 
-
-        // try {
-        //     fs.unlinkSync(path.join(__dirname, "uploads", tmpFile));
-        // } catch (error) {
-        //     // res.json(error)
-        // }
-
-        console.log("im here");
-        
-        try {
-            fs.writeFileSync(path.join(__dirname, "uploads", tmpFile), req.body.code);
-            console.log("file written");
+    try {
+        if (fs.existsSync(path.join(__dirname), "uploads")) {
+            console.log("folder exists");
+        }else{
+            fs.mkdirSync(path.join(__dirname, "uploads"))
+            console.log("folder created");
             
+        }
+        var exists = fs.existsSync(programFile);        
+    } catch (error) {
+        console.log(error);
+    }
+    if (exists) {
+        console.log("files exists: "+programFile);
+    } else {
+        console.log("file does not exists");
+        fs.writeFileSync(programFile, req.body.code, { mode: 777 })
+    }
+
+    var compiler = null;
+    var op = new Object;
+    if (lang == "c") {
+        try {
+            mid = spawnSync('gcc', ['tmp.c']);
+            console.log("file written successfully");
         } catch (error) {
             res.json(error)
         }
-        var compiler = null;
-        var op = new Object;
-        if (lang == "c") {
-            try {
-                mid = spawnSync('gcc', ['tmp.c']);
-                console.log("file written successfully");
-            } catch (error) {
-                res.json(error)
-            }
-            compiler = spawn('./a.out');
-        } else if (lang == "cpp") {
-            try {
-                mid = spawnSync('g++', ['tmp.cpp']);
-                console.log("file written successfully");
-            } catch (error) {
-                res.json(error)
-            }
-            compiler = spawn('./a.out');
+        compiler = spawn('./a.out');
+    } else if (lang == "c++") {
+        try {
+            var mid = spawnSync('g++', [programFile, "-o", path.join(__dirname, "uploads", 'tmp.out')]);            
+        } catch (error) {
+            console.log(error);
         }
-        else if (lang == "python") {
-            compiler = spawn('python3', [path.join(__dirname, "uploads/") + 'tmp.py']);
-        }
-        compiler.stdout.on('data', (data) => {
-            str = String(data)
-            console.log(`stdout: ${str}`);
-            // res.write(JSON.stringify(`process exited with code ${str}`))
-            // res.write(JSON.stringify("stdout"))
-            op.out = str;
-        });
-
-        compiler.stderr.on('data', (data) => {
-            str = String(data)
-            console.error(`stderr: ${str}`);
-            // res.write(JSON.stringify(str));
-            // res.send("stderr")
-            op.err = str;
-        });
-
-        compiler.on('close', (code) => {
-            console.log(`process exited with code ${code}`);
-            // res.end(JSON.stringify(`process exited with code ${code}`));
-            res.status(200).json(op)
-        });
-    })
+        compiler = spawn(path.join(__dirname, "uploads", './tmp.out'));
+        prepareOp(compiler, res, programFile)
+    }
+    else if (lang == "python") {
+        compiler = spawn('python3', [programFile]);
+        prepareOp(compiler,res,programFile);
+    }
 });
 
 app.listen(3000, () => {
     console.log("running on port 3000");
 })
+
+function prepareOp(compiler, res,programFile) {
+    var op = new Object;
+    compiler.stdout.on('data', (data) => {
+        str = String(data)
+        console.log(`stdout: ${str}`);
+        op.out = str;
+    });
+
+    compiler.stderr.on('data', (data) => {
+        str = String(data)
+        console.error(`stderr: ${str}`);
+        op.err = str;
+    });
+
+    compiler.on('close', (code) => {
+        op.end = `process exited with code ${code}`
+        console.log(op.end);
+        try {
+            var cleanup = fs.unlinkSync(programFile)
+            res.json(op);
+        } catch (error) {
+            console.log(error);
+        }
+    });
+    return op;
+} 
